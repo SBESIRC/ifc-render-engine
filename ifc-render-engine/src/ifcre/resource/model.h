55fd1fc75af70191c0dd699079bf28b1305938e9
THAPE\zhangzelin1 <zhangzelin1@thape.com.cn>
2022-06-30 11:03:32 +0800

    add common of  “model.h”

--- ifc-render-engine/src/ifcre/resource/model.h
+++ ifc-render-engine/src/ifcre/resource/model.h
@@ -24,13 +24,13 @@ namespace ifcre {
 	struct MtlData {
 		MtlMetaData data[8];
 		// 3 for kd(float, float, float), 3 for ks(float, float, float)
-		//1 for alpha(float), and 1 for ns(int)
+		// 1 for alpha(float), and 1 for ns(int)
 	};
 	struct MaterialData {
-		glm::vec3 kd;
-		glm::vec3 ks;
-		float alpha;
-		int ns;
+		glm::vec3 kd;		// 漫反射rgb
+		glm::vec3 ks;		// 高光项rgb(specular)
+		float alpha;		// 透明度
+		int ns;				// cos的指数(specularity)
 		MaterialData() {}
 		MaterialData(glm::vec4 a, glm::vec4 b, float c, int d) :kd(a), ks(b), alpha(c), ns(d) {}
 	};
@@ -42,7 +42,7 @@ namespace ifcre {
 			clock_t start, end;
 			start = clock();
 			
-			size_t facs = datas.face_mat.size();
+			size_t facs = datas.face_mat.size(); // 获取面的数量
 			material_data.resize(facs);
 			for (size_t i = 0; i < facs; i++) {
 				material_data[i].kd = glm::vec3(datas.face_mat[i].ka_r, datas.face_mat[i].ka_g, datas.face_mat[i].ka_b);
@@ -70,13 +70,14 @@ namespace ifcre {
 			//vertices
 			size_t s;
 			is.read((char*)&s, sizeof(size_t));
-			this->g_vertices.resize(s);
+			this->g_vertices.resize(s); // xyzxyzxyz...
 			for (int i = 0; i < s; i++) {
 				is.read((char*)&this->g_vertices[i], sizeof(Real));
 			}
+
 			//normals
 			is.read((char*)&s, sizeof(size_t));
-			this->g_normals.resize(s);
+			this->g_normals.resize(s); // xyzxyzxyz...
 			for (int i = 0; i < s; i++) {
 				is.read((char*)&this->g_normals[i], sizeof(Real));
 			}
@@ -104,7 +105,7 @@ namespace ifcre {
 
 			//material datas
 			is.read((char*)&s, sizeof(size_t));
-			material_data.resize(s);
+			material_data.resize(s); // 获取面的数量（顶点数量的1/3）
 			Vector<MtlData> mat_vec(s);
 			for (int i = 0; i < s; i++) {
 				is.read((char*)&mat_vec[i].data[0], sizeof(float) * 7);
@@ -116,11 +117,11 @@ namespace ifcre {
 					glm::vec4(mat_vec[i].data[3].f, mat_vec[i].data[4].f, mat_vec[i].data[5].f, 0),
 					mat_vec[i].data[6].f, mat_vec[i].data[7].i);
 			}
-			getVerColor();
-			generateCompIds();
-			generate_bbxs_by_comps();
-			getVerAttrib();
-			divide_model_by_alpha();
+			getVerColor();				// 生成顶点颜色数组
+			generateCompIds();			// 生成顶点到其包含物件的映射
+			generate_bbxs_by_comps();	// 生成各个物件的bbx
+			getVerAttrib();				// 生成顶点属性数组
+			divide_model_by_alpha();	// 根据透明度将顶点分为两组
 		}
 
 		Vector<uint32_t> getgIndices() {
@@ -155,26 +156,26 @@ namespace ifcre {
 			int i;
 		};
 		Vector<Real> getVerAttrib() {
-			size_t s = g_vertices.size();
+			size_t s = g_vertices.size(); //xyzxyzxyz...
 			ver_attrib.resize(s / 3 * 10);//no!
 			int offset = 0;
 			for (int i = 0; i < s; i += 3) {
-				ver_attrib[offset + i] = g_vertices[i];
-				ver_attrib[offset + i + 1] = g_vertices[i + 1];
-				ver_attrib[offset + i + 2] = g_vertices[i + 2];
-				ver_attrib[offset + i + 3] = g_normals[i];
-				ver_attrib[offset + i + 4] = g_normals[i + 1];
-				ver_attrib[offset + i + 5] = g_normals[i + 2];
-				ver_attrib[offset + i + 6] = g_kd_color[i];
-				ver_attrib[offset + i + 7] = g_kd_color[i + 1];
-				ver_attrib[offset + i + 8] = g_kd_color[i + 2];
-				ver_attrib[offset + i + 9] = util::int_as_float(comp_ids[i / 3]);//todo: transform by bit
+				ver_attrib[offset + i] = g_vertices[i];								// 位置.x
+				ver_attrib[offset + i + 1] = g_vertices[i + 1];						// 位置.y
+				ver_attrib[offset + i + 2] = g_vertices[i + 2];						// 位置.z
+				ver_attrib[offset + i + 3] = g_normals[i];							// 法向量.x
+				ver_attrib[offset + i + 4] = g_normals[i + 1];						// 法向量.y
+				ver_attrib[offset + i + 5] = g_normals[i + 2];						// 法向量.z
+				ver_attrib[offset + i + 6] = g_kd_color[i];							// 颜色.x
+				ver_attrib[offset + i + 7] = g_kd_color[i + 1];						// 颜色.y
+				ver_attrib[offset + i + 8] = g_kd_color[i + 2];						// 颜色.z
+				ver_attrib[offset + i + 9] = util::int_as_float(comp_ids[i / 3]);//todo: transform by bit // 所在物件的索引
 				offset += 7;
 			}
 			return ver_attrib;
 		}
 
-		void generateCompIds() {
+		void generateCompIds() { // comp_ids: 顶点索引找到对应的物件索引
 			comp_ids.resize(g_vertices.size()/3);
 			int j = 0;
 			for (int i = 0; i < c_indices.size(); i++) {
@@ -210,8 +211,8 @@ namespace ifcre {
 		}
 
 		void generate_bbxs_by_comps() {
-			size_t cindicessize = c_indices.size();
-			comps_bbx.resize(cindicessize * 6);
+			size_t cindicessize = c_indices.size(); // 获取物件数量
+			comps_bbx.resize(cindicessize * 6);		// 每个物件对应6个float值
 			Real a_min[3], a_max[3];
 			int bbx_offset = 0;
 			for (auto& ix : c_indices) {
@@ -233,13 +234,13 @@ namespace ifcre {
 				bbx_offset += 6;
 			}
 
+			// 整个模型的bbx
 			for (size_t i = 0; i < cindicessize; i++) {
 				for (int j = 0; j < 3; j++) {
 					a_min[j] = std::min(comps_bbx[6 * i + j], a_min[j]);
 					a_max[j] = std::max(comps_bbx[6 * i + j + 3], a_max[j]);
 				}
 			}
-
 			this->pMax = glm::vec3(a_max[0], a_max[1], a_max[2]);
 			this->pMin = glm::vec3(a_min[0], a_min[1], a_min[2]);
 			m_center = (pMin + pMax) * 0.5f;
@@ -255,7 +256,7 @@ namespace ifcre {
 					ret[i] = std::max(ret[i], comps_bbx[6 * id + i]);
 				}
 			}
-			Vector<Real> ret2(24);
+			Vector<Real> ret2(24); // 一个bbx有8个顶点，每个顶点要3个float存储位置
 			for (int i = 0; i < 8; i++) {
 				ret2[i * 3] = i & 1 ? ret[3] : ret[0];
 				ret2[i * 3 + 1] = i & 2 ? ret[4] : ret[1];
@@ -317,29 +318,28 @@ namespace ifcre {
 			return mirror_model;
 		}
 
-		uint32_t render_id;//seems a render_id combine with an array of vertex?
-		Vector<Real> ver_attrib;
-		Vector<Real> comps_bbx;//pmin, pmax
-		Vector<uint32_t> g_indices;
-		Vector<uint32_t> trans_ind;
-		Vector<uint32_t> no_trans_ind;
-		Vector<uint32_t> edge_indices;
-		Vector<Vector<uint32_t>> c_indices; // meshes` indicies with their vertices` indicies
-
-		Vector<uint32_t> bbx_drawing_order = { 0,1,5,4,0,2,6,4,5,7,3,1,3,2,6,7 };
+		uint32_t render_id;// seems a render_id combine with an array of vertex?
+		Vector<Real> ver_attrib;				// 每个顶点有10个属性，数量为顶点数量的十倍
+		Vector<Real> comps_bbx;					// pmin, pmax // 物件对应的bbx信息，数量为物件数量的6倍
+		Vector<uint32_t> g_indices;				// 顶点的索引，数量为顶点个数
+		Vector<uint32_t> trans_ind;				// 透明顶点的索引
+		Vector<uint32_t> no_trans_ind;			// 不透明顶点的索引
+		Vector<uint32_t> edge_indices;			// 
+		Vector<Vector<uint32_t>> c_indices;		// 物件->顶点的索引，1级数量为物件的个数，2级为物件拥有顶点数
+		Vector<uint32_t> bbx_drawing_order = { 0,1,5,4,0,2,6,4,5,7,3,1,3,2,6,7 }; // 按此定点顺序绘制bbx长方体框
 
 	private:
-		glm::mat4 m_model;
-		glm::mat4 m_init_model;
-		glm::mat4 mirror_model;
-		Real m_scale_factor;
-		glm::vec3 pMin, pMax;
-		glm::vec3 m_center;
-		Vector<MaterialData> material_data;
-		Vector<Real> g_vertices;
-		Vector<Real> g_kd_color;
-		Vector<Real> g_normals;
-		Vector<uint> comp_ids;
+		glm::mat4 m_model;						
+		glm::mat4 m_init_model;					
+		glm::mat4 mirror_model;					
+		Real m_scale_factor;					// 整个模型的缩放系数
+		glm::vec3 pMin, pMax;					// 整个模型的bbx信息
+		glm::vec3 m_center;						// 整个模型的中心信息
+		Vector<MaterialData> material_data;		// 存各个面的材质属性，每个面都有8项属性，数量为面的数量、顶点数量的1/3
+		Vector<Real> g_vertices;				// 依次存储各个顶点位置的x、y、z信息，数量为顶点数量的三倍
+		Vector<Real> g_kd_color;				// 依次存储各个顶点(漫反射项)颜色的x、y、z信息，数量为顶点数量的三倍
+		Vector<Real> g_normals;					// 依次存储各个顶点法向量的x、y、z信息，数量为顶点数量的三倍
+		Vector<uint> comp_ids;					// 可通过顶点索引找到对应的物件索引，数量为定点的个数
 	};
 	
 
